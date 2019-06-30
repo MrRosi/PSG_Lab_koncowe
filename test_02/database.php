1bdb2403a039978b3cc367ae762d6d011a33e9f2
MrRosi <46557781+MrRosi@users.noreply.github.com>
2019-06-30 22:38:34 +0200

    30.06.2019r.
    Produkt finalny laboratorium PSG.

---
+++ test_02/database.php
@@ -0,0 +1,8 @@
+<?php
+
+//zmienne do nawiazania polaczenie z serwerem Postgres i bazą danych test02
+	$db_host="localhost";
+	$db_port="5432";
+	$db_user="postgres";
+	$db_pass="admin"; // tutaj wpisać swoje hasło
+	$db_name="test_1";?>
---
+++ test_02/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="insert.php" method="post">
+        <h1> Formularz do komunikacji z bazą danych przestrzennych </h1><br/>
+        
+        Zmienna tekstowa<br/>
+        <input type="text" name="pole_formularz_1"/> <br/><br/>
+        Zmienna liczbowa<br/>
+        <input type="text" name="pole_formularz_2"/> <br/>
+        <h3>Dane geograficzne: </h3>
+        Długosc geograficzna <br/>
+        <input type="text" name="pole_formularz_3"/> <br/>
+        Szerokość geograficzna <br/>
+        <input type="text" name="pole_formularz_4"/> <br/>
+        
+        <input type="submit">
+        
+        
+        
+        
+    </form>
+    
+    
+    
+    
+    
+</body>
+</html>
